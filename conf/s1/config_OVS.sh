285c05a18950feaf07502d5dcdd15c2750fed450
juancarlosdiaztorres <jcdiaztorres96@gmail.com>
2018-03-27 18:30:46 +0200

    IP connection and OVS running OK, TO DO ONOS

--- conf/s1/config_OVS.sh
+++ conf/s1/config_OVS.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-echo "Open Virtual Switch is starting"
-service openvswitch-switch start
+#echo "Open Virtual Switch is starting"
+#service openvswitch-switch start
 
 
-while true; do
-      if [ -S /var/run/openvswitch/db.sock ]; then
-         break
-      fi
-      sleep 2
-done
+# while true; do
+#       if [ -S /var/run/openvswitch/db.sock ]; then
+#          break
+#       fi
+#       sleep 2
+# done
 
 
 echo "Open Virtual Switch is being configured"
-ovs-vsctl add-br s1
+#ovs-vsctl add-br s1
 ovs-vsctl add-port s1 eth1
 ovs-vsctl add-port s1 eth2
 ovs-vsctl set bridge s1 other-config:hwaddr=00:00:00:00:00:01
